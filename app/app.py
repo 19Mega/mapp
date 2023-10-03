b9d07b3c76fee6fe853d8d7ae1d97a1480364560
Matias Borba <matiasborba89@gmail.com>
2023-10-02 22:15:30 -0300

    update

--- app/app.py
+++ app/app.py
@@ -6,11 +6,19 @@ from admin import setup_admin
 from models import db, User, People, Vehicle, Planet, Favourite
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
+# Configuración de la aplicación Flask aquí
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# Configuración de la aplicación Flask aquí
+# Configuración de la base de datos SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Cambia esto al URI de tu base de datos
+db.init_app(app)
+migrate = Migrate(app, db)
 
+# Función para crear o migrar las tablas en la base de datos
+def create_or_migrate_db():
+    with app.app_context():
+        db.create_all()
 
 # Definición de endpoints de prueba
 @app.route('/')
@@ -19,14 +27,14 @@ def hello_world():
 
 @app.route('/saludo/<nombre>')
 def saludo_personalizado(nombre):
-    return jsonify(message=f'Hola, {nombre}!')
+    return jsonify(message=f'¡Hola, {nombre}!')
 
 
 
 @app.route('/signup', methods=['POST'])
 def signup():
     request_body = request.get_json(force=True)
-     
+    print(request_body)
     new_user = User(username=request_body["username"],
                     email = request_body["email"], 
                     password = request_body["password"],
@@ -39,3 +47,10 @@ def signup():
     return { "msg": "Usuario creado con éxito",
             "response": new_user.serialize(),
             }
+
+
+
+if __name__ == "__main__":
+    generate_sitemap()  # Genera el sitemap cuando se ejecuta el servidor
+    create_or_migrate_db()  # Crea o migra la base de datos al ejecutar el servidor
+    application.run()
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,11 +1,12 @@
 from app import app as application
 from utils import generate_sitemap
+from app import create_or_migrate_db  # Importa la función
 
 if __name__ == "__main__":
     generate_sitemap()  # Genera el sitemap cuando se ejecuta el servidor
+    create_or_migrate_db()  # Crea o migra la base de datos al ejecutar el servidor
     application.run()
 
-
-
-# if __name__ == "__main__":
 #     application.run(host='0.0.0.0', port=8080)
+
+
