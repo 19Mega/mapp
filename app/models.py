437987ade44a5c7a02b4a0c02e527772f0ab1c72
Matias Borba <matiasborba89@gmail.com>
2023-10-06 22:36:14 -0300

    add user table to models.py

--- app/models.py
+++ app/models.py
@@ -4,23 +4,25 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    # Relationships 1 a n con Favourite
-    # favourites = db.relationship('Favourite', backref='user', lazy=True)
-    
+    # Relationships: 1 user n favorites
+    favorites = db.relationship('Favorite', backref='user', lazy=True)  # <---
+    payments = db.relationship('Payment', backref='user', lazy=True)  # <---
 
     def __repr__(self):
-        return '<User %r>' % self.id
+        return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "username": self.username,
             "email": self.email,
+            "name": self.name,
+            "last_name": self.last_name,
             "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
